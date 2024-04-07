b6b9e3d0ccf5fd2e9d5ee1c6ef1aea6e7bbfe252
esttom <esttom42@gmail.com>
2024-04-07 22:42:32 +0900

    chore: change build dir

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,9 @@ import { presetIcons, presetUno } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs'
+  },
   plugins: [
     vue(), 
     UnoCSS({
